fddae19e895782a0c08e8eeb3b2d170d59215030
ESilverStone <68459918+ESilverStone@users.noreply.github.com>
2023-12-19 01:10:22 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	cout << "\\    /\\\n";
+	cout << " )  ( ')\n";
+	cout << "(  /  )\n";
+	cout << " \\(__)|";
+
+
+
+
+	return 0;
+}
