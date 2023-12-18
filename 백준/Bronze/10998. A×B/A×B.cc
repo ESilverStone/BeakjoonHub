4a3d40f9d7eb9e1b89e3b35cfd2c56e0d99527c7
ESilverStone <68459918+ESilverStone@users.noreply.github.com>
2023-12-19 02:24:31 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	
+	int a, b;
+	
+	cin >> a >> b;
+
+	cout << a * b;
+
+	
+
+	return 0;
+}
