ea84d8b9fd7cdabb54f04bae16c989bc11938118
ESilverStone <68459918+ESilverStone@users.noreply.github.com>
2023-12-19 02:54:48 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string s = "         ,r'\"7\n";   // \", \n 이 제어문자다.
+	s += "r`-_   ,'  ,/\n";    // \n 이 제어문자다.
+	s += " \\. \". L_r'\n";    // \\, \", \n 이 제어문자다.
+	s += "   `~\\/\n";         // \\, \n 이 제어문자다.
+	s += "      |\n";           // \n 이 제어문자다.
+	s += "      |";
+	cout << s;
+
+	return 0;
+}
