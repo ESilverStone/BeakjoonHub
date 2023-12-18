1d66cc9bd6e4df0ab563afe08640e8f70ae37f1a
ESilverStone <68459918+ESilverStone@users.noreply.github.com>
2023-12-19 00:43:52 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	cout << "Hello World!";
+
+	return 0;
+}
