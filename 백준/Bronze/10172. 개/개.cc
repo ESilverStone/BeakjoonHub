001f9fe64dc678bb5c609fc19da0379ddc1f8854
ESilverStone <68459918+ESilverStone@users.noreply.github.com>
2023-12-19 01:13:33 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	cout << "|\\_/|\n";
+	cout << "|q p|   /}\n";
+	cout << "( 0 )\"\"\"\\\n";
+	cout << "|\"^\"\`    |\n";
+	cout << "||_/=\\\\__|";
+
+	return 0;
+}
