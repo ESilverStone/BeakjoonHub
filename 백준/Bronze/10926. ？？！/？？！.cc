6dfaf3db126b548609fa310c037642d6e1de2bdc
ESilverStone <68459918+ESilverStone@users.noreply.github.com>
2023-12-24 02:08:59 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <limits>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	string str;
+
+	cin >> str;
+
+	string result = str + "??!";
+
+	cout << result;
+
+
+	return 0;
+}
