50e38f76f3b2a57f80b212524967bf4ea19ba5b4
ESilverStone <68459918+ESilverStone@users.noreply.github.com>
2024-02-28 09:38:15 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -10,19 +10,22 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
+	long long M = 1234567891;
+
 	int n;
 	cin >> n;
+	
 
 	string str;
 	cin >> str;
 
-	long sum = 0;
-	int i = 0;
+	long long sum = 0;
+	long long power = 1;
 	for (char c : str) {
 	
-		sum += (c -'a' + 1) * pow(31, i);
-		i++;
+		sum += ((c -'a' + 1) * power) % M;
+		power = (power * 31) % M;
 	}
 
-	cout << sum;
+	cout << sum % M;
 }
