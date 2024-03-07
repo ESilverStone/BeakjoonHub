ad39e13978e40e4d5fa060cdd45f0515a924ff5f
ESilverStone <68459918+ESilverStone@users.noreply.github.com>
2024-03-07 15:42:34 +0900

    [Silver II] Title: 최대 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <string>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <functional> // greater 헤더 포함
+
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    priority_queue<int, vector<int>> maxQue;
+    priority_queue<int, vector<int>, greater<int>> minQue;
+
+    int n;
+    cin >> n;
+
+    while (n--) {
+        int input;
+        cin >> input;
+
+        if (input == 0) {
+            if (maxQue.empty()) {
+                cout << 0 << "\n";
+            }
+            else {
+                cout << maxQue.top() << "\n";
+
+                maxQue.pop();
+            }
+        }
+        else {
+            maxQue.push(input);
+        }
+    }
+
+
+    return 0;
+}
